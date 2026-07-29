763e2cade06ca241b4d9da1b7cadd13f1878f9aa
TanikondaSanjay <149857238+TanikondaSanjay@users.noreply.github.com>
2026-07-29 15:55:25 +0530

    Add Single Number code

---
+++ Arrays/SingleNumber.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums){
+        for(int i=0;i<nums.length;i++){
+            int count = 0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+            if(count == 1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        SingleNumber sn = new SingleNumber();
+        System.out.println(sn.singleNumber(nums));
+    }
+}
